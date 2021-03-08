06aff21848a21b4fcf6c95acf331a38abb144f3b
Jason Lin <jasonwl_1995@yahoo.com>
2021-03-08 00:07:42 -0600

    details page not displaying, working on movieItem

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import './MovieList.css'
+import { useHistory } from 'react-router-dom';
+import './MovieList.css';
 
 function MovieList() {
 
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
+    const history = useHistory();
 
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
@@ -19,7 +21,7 @@ function MovieList() {
                     return (
                         <div key={movie.id} >
                             <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
+                            <img src={movie.poster} alt={movie.title} onClick={() => history.push(`/movie/${movie.id}`)}/>
                         </div>
                     );
                 })}
